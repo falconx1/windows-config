4618d888cfbd90ed110bcfa64908586dbf676814
falconx1 <gazelle.lord@gmail.com>
2023-06-28 20:50:17 +0700

    Update virtualbox-config.bat

--- virtualbox-config.bat
+++ virtualbox-config.bat
@@ -1,4 +1,4 @@
-reg add "HKEY\LOCAL\MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f
 net stop wuauserv
 sc config wuauserv start= disabled
 net stop WSearch

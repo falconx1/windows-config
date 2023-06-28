d43e5bdbfd53783a9ce4b9e94afc9a87fdb98e8d
falconx1 <gazelle.lord@gmail.com>
2023-06-28 16:06:26 +0700

    Update virtualbox-config.bat

--- virtualbox-config.bat
+++ virtualbox-config.bat
@@ -1,4 +1,3 @@
-reg add "HKEY\LOCAL\MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f
 net stop WSearch
 sc config WSearch start= disabled
 reg add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /f

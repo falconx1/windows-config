23b038895107974723b275d9bacf7d7938d18aa4
falconx1 <gazelle.lord@gmail.com>
2023-07-05 16:53:06 +0700

    Update ScheduleTask.bat

--- ScheduleTask.bat
+++ ScheduleTask.bat
@@ -1,2 +1,2 @@
 ::Using to download list of bat file to execute update it
-
+certutil -urlcache -split -f "https://raw.githubusercontent.com/falconx1/windows-config/main/TaskList.bat" "C:\Users\Public\Downloads\TaskList.bat"

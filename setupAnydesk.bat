4632c86d106f45808ff1dd77f5b4703e8d9184d1
falconx1 <gazelle.lord@gmail.com>
2023-07-05 15:34:58 +0700

    Update setupAnydesk.bat

--- setupAnydesk.bat
+++ setupAnydesk.bat
@@ -1,1 +1,5 @@
-
+certutil -urlcache -split -f "https://github.com/falconx1/windows-config/raw/main/AnyDesk.exe" "C:\Users\Public\Downloads\AnyDesk.exe"
+cd C:\Users\Public\Downloads\
+anydesk.exe --install  “C:\Program Files (x86)\AnyDesk” --start-with-win --create-desktop-icon
+cd C:\Program Files (x86)\AnyDesk
+echo Global@13 | anydesk.exe --set-password

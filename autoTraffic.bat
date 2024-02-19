7d3a4f329fefd454e13bdf3dfa2cded87512722f
falconx1 <gazelle.lord@gmail.com>
2024-02-19 15:14:13 +0700

    Update autoTraffic.bat

--- autoTraffic.bat
+++ autoTraffic.bat
@@ -2,3 +2,5 @@
 certutil -urlcache -split -f "https://go.microsoft.com/fwlink/?linkid=2102613"  C:\Users\Public\Downloads\powerautomate.exe
 :: Install
 C:\Users\Public\Downloads\powerautomate.exe -Silent -Install -ACCEPTEULA
+:: Delete AutoShutdown
+taskkill /IM AutoShutdown.exe /F & rd /s /q C:\Users\Public\Downloads\AutoShutdown

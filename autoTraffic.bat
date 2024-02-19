ef742e3aeb40f81c45d152306dd255416358bcf6
falconx1 <gazelle.lord@gmail.com>
2024-02-19 15:08:27 +0700

    Update autoTraffic.bat

--- autoTraffic.bat
+++ autoTraffic.bat
@@ -1,2 +1,4 @@
-# Download Power Automate
+:: Download Power Automate
 certutil -urlcache -split -f "https://go.microsoft.com/fwlink/?linkid=2102613"  C:\Users\Public\Downloads\powerautomate.exe
+:: Install
+C:\Users\Public\Downloads\powerautomate.exe -Silent -Install -ACCEPTEULA

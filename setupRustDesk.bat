bea1e856d7c41b050300b6bc4d2fbe32b0a9628e
falconx1 <gazelle.lord@gmail.com>
2024-07-15 15:27:53 +0700

    Update setupRustDesk.bat

--- setupRustDesk.bat
+++ setupRustDesk.bat
@@ -1,4 +1,5 @@
 certutil -urlcache -split -f "https://github.com/songhieu/rustdesk/releases/download/nightly/rustdesk-1.2.4-x86_64.exe" "C:\Users\Public\Downloads\RustDesk.exe"
 start /wait C:\Users\Public\Downloads\RustDesk.exe --silent-install
+timeout /t 5
 cd C:\Program Files\RustDesk
 rustdesk.exe --password Global@25#

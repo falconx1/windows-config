d5a3406ee7e866efaeac1c884a28d81611367173
falconx1 <gazelle.lord@gmail.com>
2025-03-27 15:45:59 +0700

    Create installRustdesk.bat

---
+++ installRustdesk.bat
@@ -0,0 +1,10 @@
+taskkill /f /im rustdesk.exe >nul 2>&1
+rmdir /s /q "C:\Users\%USERNAME%\AppData\Roaming\RustDesk"
+rmdir /s /q "C:\Program Files\RustDesk"
+certutil -urlcache -split -f "https://aws-canawan-files.sgp1.cdn.digitaloceanspaces.com/rustdesk/rustdesk-1.3.9-x86_64-sidcorp.exe" "C:\Users\Public\Downloads\RustDesk.exe"
+start /wait C:\Users\Public\Downloads\RustDesk.exe --silent-install
+timeout /t 5
+cd C:\Program Files\RustDesk
+rustdesk.exe --password Global@25#
+::init get id
+certutil -urlcache -split -f "https://raw.githubusercontent.com/falconx1/windows-config/main/getRustDeskid.bat" "C:\Users\Public\Downloads\getRustDeskid.bat"
